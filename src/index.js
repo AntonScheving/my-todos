71cbf18c1286ae17c53dc649c335a25596f29e86
AntonScheving <115781123+AntonScheving@users.noreply.github.com>
2023-03-06 22:23:43 +0000

    Added localStorage

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // https://reactjs.org/docs/strict-mode.html
   <React.StrictMode>
     <App />
   </React.StrictMode>
