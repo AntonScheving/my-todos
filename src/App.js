0088c21e7617f8dae9292f9f929d94bbf7a21c8c
AntonScheving <115781123+AntonScheving@users.noreply.github.com>
2023-03-06 18:27:30 +0000

    Added input, and buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos ] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList />
+      <input type="text" />
+      <button> Add todo</button>
+      <button>Clear completed</button>
+      <div>0 left to do</div>
+    </>
   );
 }
 
