603718d39b006e0329057d3abf8e6f4067af3acc
AntonScheving <115781123+AntonScheving@users.noreply.github.com>
2023-03-06 19:53:43 +0000

    Added a checkbox

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false }])
   return (
     <>
       <TodoList todos={todos} />
